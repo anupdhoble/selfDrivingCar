bfb6a003626622de9fb9cec657758e81960dca2e
Anup Dhoble <83175840+anupdhoble@users.noreply.github.com>
2023-12-28 00:57:54 +0530

    Collison detection , traffic simulaton
    Traffic Simulation is added, car collision detection added using polygon corners and get intersection function

--- sensor.js
+++ sensor.js
@@ -11,17 +11,17 @@ class Sensor{
 
     }
 
-    update(roadBorders){
+    update(roadBorders,traffic){
         this.#castRays();
         this.readings=[];
         for(let i=0;i<this.rays.length;i++){
             this.readings.push(
-                this.#getReading(this.rays[i],roadBorders)
+                this.#getReading(this.rays[i],roadBorders,traffic)
             );
         }
     }
 
-    #getReading(ray,roadBorders){
+    #getReading(ray,roadBorders,traffic){
         let touches=[]; //all touchpoints where ray touches something
 
         for(let i=0;i<roadBorders.length;i++){
@@ -33,6 +33,20 @@ class Sensor{
             )
             if(touch){
                 touches.push(touch);
+            }
+        }
+        for(let i=0;i<traffic.length;i++){
+            const poly=traffic[i].polygon;
+            for(let j=0;j<poly.length;j++){
+                const value=getIntersection(
+                    ray[0],
+                    ray[1],
+                    poly[j],
+                    poly[(j+1)%poly.length]
+                );
+                if(value){
+                    touches.push(value);
+                }
             }
         }
         if(touches.length==0){
