7a87d9a735edfa5f4a208ea2ecd8782c30925c7e
Anup Dhoble <83175840+anupdhoble@users.noreply.github.com>
2023-12-29 13:18:32 +0530

    pararrelization included, simulated nearly 100's of cars that our neural network drives,

--- main.js
+++ main.js
@@ -6,35 +6,64 @@ networkCanvas.width=300;
 const carCtx =carCanvas.getContext("2d");
 const networkCtx =networkCanvas.getContext("2d");
 const road = new Road(carCanvas.width/2,carCanvas.width*0.9);
-const car= new Car(road.getLaneCenter(1),100,30,50,"AI");
+
+const N=100;
+const cars=generateCars(N);
+
+
 const traffic=[
     new Car(road.getLaneCenter(1),-100,30,50,"DUMMY",1)
 ];//traffic are cars in array
 
 animate();
 
+
+
+function generateCars(N){
+    const cars=[];
+    for(let i=1;i<=N;i++){
+        cars.push(new Car(road.getLaneCenter(1),100,30,50,"AI"));
+    }
+    return cars;
+}
  
 function animate(time){
     for(let i=0;i<traffic.length;i++){
         traffic[i].update(road.borders,[]);
     }
-    car.update(road.borders,traffic);
+    for(let i=0;i<cars.length;i++){
+        cars[i].update(road.borders,traffic);
+    }
+
+    const bestCar = cars.find(
+        c=>c.y==Math.min(
+            ...cars.map(c=>c.y) // there is need to spread cars array as Math function dont support arrays
+        )
+    );
+
     carCanvas.height=window.innerHeight;
     networkCanvas.height=window.innerHeight;
 
     carCtx.save();
-    carCtx.translate(0,-car.y+carCanvas.height*0.7);
+    carCtx.translate(0,-bestCar.y+carCanvas.height*0.7);
 
     road.draw(carCtx);
     for(let i=0;i<traffic.length;i++){
         traffic[i].draw(carCtx,"red");
     }
-    car.draw(carCtx,"blue");
+
+    carCtx.globalAlpha=0.2;
+    for(let i=0;i<cars.length;i++){
+        cars[i].draw(carCtx,"blue");
+    }
+
+    carCtx.globalAlpha=1;
+    bestCar.draw(carCtx,"blue",true);
 
     carCtx.restore();
 
     networkCtx.lineDashOffset=-time/50;
-    Visualizer.drawNetwork(networkCtx,car.brain);
+    Visualizer.drawNetwork(networkCtx,bestCar.brain);
 
 
     requestAnimationFrame(animate); //calls animate function again and again;
